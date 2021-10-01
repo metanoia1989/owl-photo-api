f9d4d78bd2aa46813fdfe3947e1e24ef804e6a8b
Adam Smith <sogaxili@gmail.com>
2021-10-02 03:02:44 +0800

    fix: 修正graphql query定义错误问题

--- src/graphql/resolvers/uploadResolver.ts
+++ src/graphql/resolvers/uploadResolver.ts
@@ -3,11 +3,11 @@ import { GraphQLUpload } from 'graphql-upload'
 
 
 export default {
-  Query: {
-    // async getUploads(_, {}) {
-    //   // return await this.getUploads.find()
-    // },
-  },
+  // Query: {
+  //   async getUploads(_, {}) {
+  //     // return await this.getUploads.find()
+  //   },
+  // },
   Mutation: {
     async singleUpload(_, { file }) {
       console.log('接收到了请求', file)
